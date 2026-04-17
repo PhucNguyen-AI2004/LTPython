f510c0c48493d192af8d3240810ffd13f1d0c642
PhucNguyen-AI2004 <phucit.online@gmail.com>
2026-04-17 10:13:04 +0700

    upload source code

---
+++ cd_cr_chieucaoHCN.py
@@ -0,0 +1,21 @@
+dai = float(input(" Nhập chiều dài đáy hình chữ nhật (cm): " ))
+rong = float(input(" Nhập chiều rộng đáy hình chữ nhật (cm): "))
+cao = float(input(" Nhập chiều cao hình khối chữ nhật (cm): "))
+decimal=2
+dien_tich = dai * rong
+the_tich = dien_tich * cao
+
+print("Diện tích đáy hình chữ nhật = {0:.{2}f} cm²".format(dien_tich, the_tich, decimal))
+print("Thể tích hình khối = {0:.{1}f} cm³".format(the_tich,decimal))
+
+
+
+dai = eval(input(" Nhập chiều dài đáy hình chữ nhật (cm): " ))
+rong = eval(input(" Nhập chiều rộng đáy hình chữ nhật (cm): "))
+cao = eval(input(" Nhập chiều cao hình khối chữ nhật (cm): "))
+decimal=2
+dien_tich = dai * rong
+the_tich = dien_tich * cao
+
+print("Diện tích đáy hình chữ nhật = {0:.{2}f} cm²".format(dien_tich, the_tich, decimal))
+print("Thể tích hình khối = {0:.{1}f} cm³".format(the_tich,decimal))
